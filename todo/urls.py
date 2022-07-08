33b12b86a591edd8b2a2792b58722da906dc6a27
ArkTZ <temirlan.2735@gmail.com>
2022-07-08 09:12:33 +0600

    DB acsess to users

--- todo/urls.py
+++ todo/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     # Todo
     path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
+    path('create/', views.createtodo, name='createtodo'),
 
 ]
