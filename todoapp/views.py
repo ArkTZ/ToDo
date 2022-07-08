e8edb1f2938cbfd1f5fffe35553e09f76f0a563f
ArkTZ <temirlan.2735@gmail.com>
2022-07-08 08:05:20 +0600

    auth ready

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+
+
+def home(request):
+    return render(request, 'todoapp/home.html')
+
 
 def signupuser(request):
     if request.method == 'GET':
-        return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm()})
+        return render(request, 'todoapp/signupuser.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
@@ -15,10 +20,19 @@ def signupuser(request):
                 login(request, user)
                 return redirect('currenttodos')
             except IntegrityError:
-                return render(request, 'todoapp/signupuser.html',{'form': UserCreationForm(), 'error': 'The user name already exist. Please choose a new username'})
+                return render(request, 'todoapp/signupuser.html', {'form': UserCreationForm(),
+                                                                   'error': 'The user name already exist. Please choose a new username'})
 
         else:
-            return render(request, 'todoapp/signupuser.html', {'form': UserCreationForm(), 'error': 'Password didnt match'})
+            return render(request, 'todoapp/signupuser.html',
+                          {'form': UserCreationForm(), 'error': 'Password didnt match'})
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
 
 def currenttodos(request):
     return render(request, 'todoapp/currenttodos.html')
